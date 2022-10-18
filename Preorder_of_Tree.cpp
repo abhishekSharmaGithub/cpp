6b164fc60777dcec9f6dfeca90d79a2b5a8415e9
Dellucifer <84488210+dellucifer@users.noreply.github.com>
2022-10-18 09:32:18 +0530

    Added Preorder of Tree

---
+++ Preorder_of_Tree.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            } else {
+                Node* cur;
+                if(data <= root->data) {
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                } else {
+                    cur = insert(root->right, data);
+                    root->right = cur;
+               }
+
+               return root;
+           }
+        }
+
+/* you only have to complete the function given below.  
+Node is defined as  
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+*/
+
+    void preOrder(Node *root) {
+        if(root != NULL){              // NULL not null
+            cout<< (root->data)<<" ";
+            preOrder(root->left);    // print the left of the tree
+            preOrder(root->right);  // print the right of the tree
+        }
+    }
+
+}; //End of Solution
